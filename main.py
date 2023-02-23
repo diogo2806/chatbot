090a7c8912cd215b2d9692f9209dec60d3bddb95
diogo2806 <diogo_2806@hotmail.com>
2023-02-23 19:26:30 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, request, jsonify
 
-load_dotenv() #teste123
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_folder=".", static_url_path="")
+conversations = {}
 
-def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequency_penalty=0, presence_penalty=0):
+def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequency_penalty=0, presence_penalty=0, conversation_id=None):
     completions = openai.Completion.create(
         engine=model,
         prompt=prompt,
@@ -18,6 +19,8 @@ def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequen
         top_p=top_p,
         frequency_penalty=frequency_penalty,
         presence_penalty=presence_penalty,
+        prompt_suffix=f"\nConversation ID: {conversation_id}" if conversation_id else None,
+        stop=["\n"],
     )
     message = completions.choices[0].text
     return message.strip()
@@ -29,4 +32,22 @@ def index():
 @app.route("/chat", methods=["POST"])
 def chat():
     message = request.json.get("message")
-    response
+    conversation_id = request.json.get("conversation_id")
+    
+    if conversation_id is None:
+        conversation_id = str(len(conversations))
+        conversations[conversation_id] = {"prompt": "", "response": ""}
+
+    conversations[conversation_id]["prompt"] += f"{message}\n"
+    response = ask_openai(
+        prompt=conversations[conversation_id]["prompt"], 
+        model="text-davinci-003", 
+        max_tokens=200, 
+        conversation_id=conversation_id
+    )
+    conversations[conversation_id]["response"] += f"{response}\n"
+    
+    return jsonify({"answer": response, "conversation_id": conversation_id})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
